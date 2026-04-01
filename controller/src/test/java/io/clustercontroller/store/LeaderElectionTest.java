e758bff335ce90c28e9462a8c8527aa9dec50639
Michael Froh <msfroh@apache.org>
2026-04-01 12:45:03 -0700

    Fix build
    Needed to update netty dependency SHAs and address Mockito
    UnnecessaryStubbingException.
    
    Signed-off-by: Michael Froh <msfroh@apache.org>

--- controller/src/test/java/io/clustercontroller/store/LeaderElectionTest.java
+++ controller/src/test/java/io/clustercontroller/store/LeaderElectionTest.java
@@ -9,9 +9,9 @@ import io.etcd.jetcd.support.CloseableClient;
 import io.grpc.stub.StreamObserver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.*;
 /**
  * Tests for leader election functionality in EtcdMetadataStore
  */
-@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class LeaderElectionTest {
 
     @Mock
